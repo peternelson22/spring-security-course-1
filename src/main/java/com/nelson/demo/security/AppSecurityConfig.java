62ffedbd85875132c3c80dbd21a4b4bb6d1cf3f4
PeteNelson22 <peternelson063@gmail.com>
2022-10-02 22:24:25 +0200

    Added DaoProvider

--- src/main/java/com/nelson/demo/security/AppSecurityConfig.java
+++ src/main/java/com/nelson/demo/security/AppSecurityConfig.java
@@ -1,21 +1,21 @@
 package com.nelson.demo.security;
 
+import com.nelson.demo.auth.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.nelson.demo.security.AppUserRole.*;
+import static com.nelson.demo.security.AppUserRole.STUDENT;
 
 @Configuration
 @EnableWebSecurity
@@ -25,9 +25,12 @@ public class AppSecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
 
+    private  final AppUserService appUserService;
+
     @Autowired
-    public AppSecurityConfig(PasswordEncoder passwordEncoder) {
+    public AppSecurityConfig(PasswordEncoder passwordEncoder, AppUserService appUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.appUserService = appUserService;
     }
 
     @Bean
@@ -73,6 +76,22 @@ public class AppSecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+
+
+
+   /* @Bean     THIS WAS BEFORE USERDETAILSSERVICE WAS IMPLEMENTED
     protected UserDetailsService userDetailsService(){
         UserDetails nel = User.builder()
                 .username("nel")
@@ -96,5 +115,5 @@ public class AppSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(nel, tan, pet);
-    }
+    }*/
 }
